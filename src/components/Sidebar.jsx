0890064e19caff3b0512a678da944d9f9ca7c1d3
kamlesh-cyber <kamleshgupta2804@gmail.com>
2023-11-21 08:03:08 +0530

    login submit done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,15 +12,37 @@ const Sidebar = ({val}) => {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  var flag = 0;
+  const handleSubmit = () => {
+    const email = document.getElementById('email');
+    const pass = document.getElementById('password');
+    if(email.value != "" && pass.value !=""){
+      setShow(false);
+      flag=1;
+    }
+    else{
+      flag=0;
+    }
+  }
+  const generes = document.getElementById('generes');
+  const addMovies = document.getElementById('addMovies');
+  if(flag===1){
+    document.getElementById('generes').style({"display": "block"});
+    document.getElementById('addMovies').style({"display": "block"});
+  }else{
+    document.getElementById('generes').style({"display": "none"});
+    document.getElementById('addMovies').style({"display": "none"});
+  }
+
 	const imageUrl =
     "https://static.toiimg.com/thumb/msid-90065339,imgsize-28018,width-400,resizemode-4/90065339.jpg";
 	return (
     <div className="topDiv">
       <div className="sideBar">
-        <div className="tab">Home</div>
-        <div className="tab">Movies</div>
-        <div className="tab">Generes</div>
-        <div className="tab">Add Movies</div>
+        <div className="tab" id="home">Home</div>
+        <div className="tab" id="movies">Movies</div>
+        <div className="tab" id="generes">Generes</div>
+        <div className="tab" id="addMovies">Add Movies</div>
       </div>
       <div className="middleDiv">
         <h2>Find a Movie to watch tonight!</h2>
@@ -44,7 +66,9 @@ const Sidebar = ({val}) => {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>
+            Login
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div
@@ -61,6 +85,7 @@ const Sidebar = ({val}) => {
             <h3>Login Here!</h3>
             <input
               type="email"
+              id="email"
               placeholder="Enter Email Id"
               style={{
                 borderRadius: "10px",
@@ -70,6 +95,7 @@ const Sidebar = ({val}) => {
             />
             <input
               type="password"
+              id="password"
               placeholder="Enter Passsword"
               style={{
                 borderRadius: "10px",
@@ -78,11 +104,12 @@ const Sidebar = ({val}) => {
               }}
             />
             <button
-              type="submit"
+              type="button"
+              onClick={handleSubmit}
               style={{
                 width: "300px",
                 height: "30px",
-				marginBottom: "30px",
+				        marginBottom: "30px",
                 background:
                   "linear-gradient(91deg, #FFA300 5.73%, #FF6E00 100%)",
                 border: "none",
